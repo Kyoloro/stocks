a926df2306b3d76b24b09aa72db8cf96221a748c
李鹏程 <379623292@qq.com>
2016-10-08 00:34:20 +0800

    style: remove superfluous code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stockes
 一个终端看新浪股票的小程序
+```javascript
+node app.js
+```
--- get_mini/test3.js
+++
@@ -1,7 +0,0 @@
-var http =require('http');
-var url = 'http://www.mi.com/index.html';
-http.get(url,function(res){
- 		res.on('data',function(chunk){
- 			console.log(chunk.toString());
- 		})
-	});
