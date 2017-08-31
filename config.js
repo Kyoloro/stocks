895f77eef3b242bd26653f2a4864b0c25f359cfd
李鹏程 <379623292@qq.com>
2017-08-31 14:29:08 +0800

    update example

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-module.exports = {
-    stockIdList: [] // 输入A股列表
-}
+// module.exports = {
+//     stockIdList: [] // 输入A股列表
+// }
 
 // for example
 // sh600150 沪A-中国船舶 sh600684
-// module.exports = {
-//     stockIdList: ['sh600150', 'sh600684', 'sz300059', 'sz300168']
-// }
+module.exports = {
+    stockIdList: ['sh600150', 'sh600684', 'sz300059', 'sz300168']
+}
